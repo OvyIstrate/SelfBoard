c3ed10a79a201973946d08de1b9fdacc3d9b337e
OvyIstrate <ovy.istrate@gmail.com>
2017-03-05 18:49:01 +0200

    changes

--- js/mail.js
+++ js/mail.js
@@ -1,33 +1,36 @@
+var emailCount = 0;
 
-    var emailCount = 0;
+function emailCallBack(message) {
+    alert(message);
+    emailCount += 1;
+}
 
-    function emailCallBack(message) {
-        alert(message);
-        emailCount += 1;
+$("#mailForm").submit(function() {
+    alert("Your message has been sent");
+});
+
+function sendEmail() {
+    var name = $("#input-name").val();
+    var email = $("#input-mail").val();
+    var message = $('#input-message').val();
+
+    if (emailCount < 10) {
+        var requestModel = {
+            name: name,
+            email: email,
+            message: message
+        };
+
+        var requestUrl = "http://www.selfboardapi.com/api/mail";
+        // $.post(requestUrl, requestModel, emailCallBack);
+        var options = {
+            method: "OPTIONS",
+            url: requestUrl,
+            data: requestModel,
+            contentType: "json"
+        };
+        $.ajax(options).success(function(response) {
+            alert(response);
+        });
     }
-
-    function sendEmail() {
-        var name = $("#input-name").val();
-        var email = $("#input-mail").val();
-        var message = $('#input-message').val();
-
-        if(emailCount < 10){
-            var requestModel = {
-                name: name,
-                email: email,
-                message: message
-            };
-
-            var requestUrl = "http://www.selfboardapi.com/api/mail";
-            // $.post(requestUrl, requestModel, emailCallBack);
-            var options = {
-                method:"OPTIONS",
-                url:requestUrl,
-                data:requestModel,
-                contentType:"json"
-            };
-            $.ajax(options).success(function(response){
-                alert(response);
-            });
-        }
-    }
+}
